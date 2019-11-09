9ff440d4baa42407780cd253705521be4197372a
ewoodwell <ewoodwell@gmail.com>
2019-11-09 17:24:07 -0500

    home page first draft

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,7 +1,13 @@
 <template>
-    <div id="home">
-        testing
-    </div>
+        <div id="home">
+            <div id="title">
+                <h1>Hawking</h1>
+                <h2 id=""> Accessible Browser </h2>
+                <router-link :to="{name: 'Pair'}">
+                    <button onclick="Pair()" id="Pair"> Pair </button>
+                </router-link>
+            </div>
+        </div>
 </template>
 
 <script>
@@ -14,5 +20,34 @@ export default {
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+    #home {
+    }
+
+    #title {
+        margin: 0;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    h1 {
+        font-size: 50px;
+    }
+
+    h2 {
+        font-size: 40px;
+    }
+
+    #Pair {
+        height: 5vw;
+        width: 7vw;
+        color: white;
+        background-color: cornflowerblue;
+        font-size: 30px;
+        font-style: initial;
+        border-style: none;
+        border-radius: 15%;
+    }
 
 </style>
