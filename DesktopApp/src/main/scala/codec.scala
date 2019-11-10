7128ea8213b3a59152509b2d359fa0cf66ff074e
Shawn TheGuy <shawn.dermer@gmail.com>
2019-11-09 19:49:30 -0500

    trait added

--- DesktopApp/src/main/scala/codec.scala
+++ DesktopApp/src/main/scala/codec.scala
@@ -2,11 +2,13 @@ import play.api.libs.json._
 import play.api.libs.functional.syntax._
 import play.socketio.scaladsl.SocketIOEventCodec._
 
-case class Flag(message: String)
+sealed trait HawkingEvent
+
+case class Flag(message: String) extends HawkingEvent
 object Flag {
   implicit val format: Format[Flag] = Json.format
 }
 
 val decoder = decodeByName {
-  case "start response" => decodeJson[String]
+  case "flag" => decodeJson[String]
 }
